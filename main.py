d96b27ec6292208ebbd2d98553007edc30e33bd4
鸡毛 <shihao.gu@perfma.com>
2019-01-07 14:39:09 +0800

    调整拟合思路

--- main.py
+++ main.py
@@ -4,8 +4,19 @@
 import numpy as np;
 from bpNet.bpNet import bpNet;
 
-network = bpNet(7, [32, 16], 1);
+def getPoints (r, x, y, z, size):
+    thet = np.random.rand(size) * np.pi * 2;
+    fai = np.random.rand(size) * np.pi;
+    x = x + r * np.sin(thet) * np.cos(fai);
+    y = y + r * np.sin(thet) * np.sin(fai);
+    z = z + r * np.cos(thet);
+    result = np.array([x, y, z]);
+    result = result.transpose((1, 0));
+    return result;
+inputs = getPoints(88, 34, 111, 51, 100);
 
-input = np.array([1, 2, 3, 4, 5, 6, 7]);
-result = network.predict(input);
+# 新建一个网络，输入为三维坐标，输出为拟合的球体方程（r, x, y, z）
+network = bpNet(3, [32, 16], 4);
+
+result = network.predict(inputs);
 print(result);
